70dbd190d23979766646a480a3ae85eeac7903c3
IvaTsu <ivan.tsud@gmail.com>
2020-03-05 17:56:25 +0200

    Refactor combiner to be async with callbacks

--- hooks/combiner.js
+++ hooks/combiner.js
@@ -1,25 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
-// PATH to the hooks/ directory
+// PATH to the "hooks/" directory
 const dir = `${__dirname}`;
-console.log(dir);
+// Output filename for the "combined hooks" file
+const outFile = "combiner_hook.json";
+// Output PATH for the "combined hooks" file
+const res = `${outFile}`;
 
+// Temporary container for the data from separate hooks
 const data = [];
 
+// Read directory
 fs.readdir(dir, (err, files) => {
-  return new Promise((resolve, reject) => {
-    if (err) reject(err);
-    files.forEach(file => {
-      console.log(file);
-      if (path.extname(file) === ".json" && file !== "combiner_hook.json") {
-        const x = fs.readFileSync(file);
-        data.push(x.toString("utf-8").slice(1, -2));
-        console.log(data);
-      }
-    });
-    resolve(data);
-  }).then(data => {
-    fs.writeFileSync("combiner_hook.json", "[" + data + "\n]");
+  if (err) throw (err);
+  files.forEach(file => {
+    if (path.extname(file) === ".json" && file !== outFile) {
+      // Read file
+      fs.readFile(file, (err, fileContent) => {
+        if (err) throw err;
+        // Write file
+        data.push(fileContent.toString("utf-8").slice(1, -2));
+        fs.writeFile(res, "[" + data + "\n]", err => {
+          if (err) throw err;
+        });
+      });
+    }
   });
 });
